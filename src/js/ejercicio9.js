6a5c74b25c62ef85f061f6d2f881b483e0ee2b29
sarariveros <sara.riverosd@gmail.com>
2022-10-30 17:12:15 -0500

    prueba

--- src/js/ejercicio9.js
+++ src/js/ejercicio9.js
@@ -1,17 +1,17 @@
-function arreglo (str){
-    
+function arreglo(str) {
+
     const num = document.getElementById(str).value
 
-    let suma = 0
+    let suma = 2
 
     for (let i = 1; i <= num; i++) {
 
         suma = suma + i;
-        
+
     }
 
     document.getElementById('result').innerHTML = suma
-    
+
 }
 
 export default arreglo
