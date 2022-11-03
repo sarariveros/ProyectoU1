2b6388c2ed7b5bf288865f36b0d490e4aeb7dcde
Javier Prado <pradojavier930@gmail.com>
2022-11-02 20:25:35 -0500

    cambiosV-3

--- main.js
+++ main.js
@@ -2,8 +2,24 @@ import './style.css'
 import './src/css/union.css'
 
 // como modulo
+let dato=1
 import iniciar from './src/js/iniciar'
 
-
+//import cambiar from "./src/js/vista_3"
+import {cambiarNivel1,cambiarNivel2,cambiarNivel3, playAudio, setDarkMode, setLigthMode} from "./src/js/vista_3";
 // iniciamos
 iniciar()
+
+//vista 3
+cambiarNivel1()
+cambiarNivel2()
+cambiarNivel3()
+setDarkMode()
+setLigthMode()
+playAudio()
+// const elemento =document.getElementById("switch").value
+// console.log(elemento);
+// //Creando la musica
+// let audio = document.getElementById("audio");
+
+
--- src/js/iniciar.js
+++ src/js/iniciar.js
@@ -2,7 +2,7 @@ import template1 from '../html/vista_1.html?raw'
 import template2 from '../html/vista_2.html?raw'
 import template3 from '../html/vista_3.html?raw'
 import template4 from '../html/vista_4.html?raw'
-
+import template6 from '../html/vista_6.html?raw'
 function iniciar(){
 
     // cargamos el html
@@ -10,7 +10,8 @@ function iniciar(){
     document.getElementById('vista_2').innerHTML = template2
     document.getElementById('vista_3').innerHTML = template3
     document.getElementById('vista_4').innerHTML = template4
-
+    document.getElementById('vista_6').innerHTML = template6
+    
 }
 
 
