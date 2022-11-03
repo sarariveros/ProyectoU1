60ffb529d59f66f4dbd49bf6ceeab9cf72a868a4
sarariveros <sara.riverosd@gmail.com>
2022-11-02 20:28:37 -0500

    Actualizacion_Sara

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ import './src/css/union.css'
 
 // como modulo
 import iniciar from './src/js/iniciar'
+import iniciarVista4 from './src/js/vista_4'
+import iniciarVista1 from './src/js/vista_1'
+//import json from './src/js/sara/ranking.json'
 
 
 // iniciamos
+
 iniciar()
+iniciarVista4();
+iniciarVista1();
--- src/js/iniciar.js
+++ src/js/iniciar.js
@@ -2,14 +2,16 @@ import template1 from '../html/vista_1.html?raw'
 import template2 from '../html/vista_2.html?raw'
 import template3 from '../html/vista_3.html?raw'
 import template4 from '../html/vista_4.html?raw'
+import template5 from '../html/vista_5.html?raw'
 
-function iniciar(){
+function iniciar() {
 
     // cargamos el html
     document.getElementById('vista_1').innerHTML = template1
     document.getElementById('vista_2').innerHTML = template2
     document.getElementById('vista_3').innerHTML = template3
     document.getElementById('vista_4').innerHTML = template4
+    document.getElementById('vista_5').innerHTML = template5
 
 }
 
