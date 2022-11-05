dd76d3cd5cd761c8397b47db0b01710d8815f92f
sarariveros <sara.riverosd@gmail.com>
2022-11-04 19:47:43 -0500

    cambios 90% sara

--- src/js/vista_1.js
+++ src/js/vista_1.js
@@ -1,12 +1,78 @@
+const l_usu = [
+    {
+        usuario: 'doc1',
+        password: '123abc',
+        rol: '1'
+    },
+    {
+        usuario: 'est1',
+        password: '',
+        rol: '0'
+    },
+]
 function iniciarVista1() {
+    document.getElementById('result_pass').innerHTML = '';
+
+    const select_rol = document.getElementById('rol');
+    select_rol.addEventListener("change", activarPass);
     const btn_guardar = document.getElementById('btn-guardar');
     btn_guardar.addEventListener("click", V1_guardar);
+
+
+
+}
+function activarPass() {
+    const password = document.getElementById('password');
+    console.log('en activar');
+    switch (this.value) {
+        case "0": password.disabled = true;
+
+            break;
+
+        case "1": password.disabled = false;
+            break;
+    }
+
+
 }
 function V1_guardar() {
+    //limpiando
+    document.getElementById('result_pass').innerHTML = '';
+    document.getElementById('result_usu').innerHTML = '';//para limpiar
 
-    const nombre = document.getElementById('inp-nombre').value;
-    document.getElementById('v1_jugador').innerText = nombre;
-    console.log('aaaa---', nombre);
+    // validando usuario no vacio
+
+    let usuario = document.getElementById('usuario').value;
+    if (usuario == '') {
+        // usuario = 'Anonimo';
+        document.getElementById('result_usu').innerHTML = 'Debes ingresar <strong>Usuario</strong>';
+    }
+
+    //validando password no vacio
+    const rol = document.getElementById('rol').value;
+    let password = document.getElementById('password').value;
+    if (rol == '1' && password == '') {
+        document.getElementById('result_pass').innerHTML = 'Debe ingresar <strong>contraseña</strong> o cambiar a rol <strong>Estudiante</strong>';
+
+    }
+    //verificando datos
+
+    let usuarios = [...l_usu];
+    usuarios.forEach(element => {
+        if (element.usuario == usuario && element.password == password && rol == element.rol) {
+            console.log('ingresaste');
+            return;
+        }
+        else { document.getElementById('result_pass').innerHTML += '</br>Datos incorrectos' }
+
+
+    });
+
+
 
 }
+
+
+
+
 export default iniciarVista1
--- src/js/vista_4.js
+++ src/js/vista_4.js
@@ -1,16 +1,100 @@
 //import cargarRanking from './sara/v4_funcion'
 // import data from '/ranking.json'
-const nombre = document.getElementById('v1_jugador');
-const nivel = document.getElementById('v1_nivel');
-const tiempo = document.getElementById('v1_tiempo');
+// const nombre = document.getElementById('v1_jugador');
+// const nivel = document.getElementById('v1_nivel');
+// const tiempo = document.getElementById('v1_tiempo');
+//bd ranking
+const l_rank = [
+    {
+        usuario: 'sara',
+        nivel: '1',
+        tiempo: '320'
+    },
+    {
+        usuario: 'jeremy',
+        nivel: '1',
+        tiempo: '195'
+    },
+    {
+        usuario: 'javier',
+        nivel: '1',
+        tiempo: '490'
+    },
+    {
+        usuario: 'pablo',
+        nivel: '1',
+        tiempo: '372'
+    },
+    {
+        usuario: 'gonzalo',
+        nivel: '1',
+        tiempo: '250'
+    }, {
+        usuario: 'ana',
+        nivel: '2',
+        tiempo: '420'
+    },
+    {
+        usuario: 'jeremyz',
+        nivel: '2',
+        tiempo: '560'
+    },
+    {
+        usuario: 'javiercito',
+        nivel: '2',
+        tiempo: '685'
+    },
+    {
+        usuario: 'pablote',
+        nivel: '2',
+        tiempo: '750'
+    },
+    {
+        usuario: 'gonza',
+        nivel: '2',
+        tiempo: '890'
+    },
+    {
+        usuario: 'usu1',
+        nivel: '3',
+        tiempo: '950'
+    },
+    {
+        usuario: 'usu2',
+        nivel: '3',
+        tiempo: '1500'
+    },
+    {
+        usuario: 'usu3',
+        nivel: '3',
+        tiempo: '1260'
+    },
+    {
+        usuario: 'usu4',
+        nivel: '3',
+        tiempo: '1560'
+    },
+    {
+        usuario: 'usu5',
+        nivel: '3',
+        tiempo: '1350'
+    },
+]
+//cargando BD
+let l_ranking;
+
+
+
 function iniciarVista4() {
+    //iniciar tabla
+    actualizarR(1);
 
 
     ////nivel
-
     const btn_nivel = document.querySelectorAll('input.btn-check');
+    var opciones = [].slice.call(btn_nivel);
 
-    btn_nivel.forEach(element => {
+    opciones.forEach(element => {
         element.addEventListener("click", cambiarNivel);
     });
 
@@ -19,22 +103,59 @@ function iniciarVista4() {
 function cambiarNivel() {
     const header = document.getElementsByTagName('th');
     header.item(0).innerHTML = 'N' + this.value;
-    actualizar(this.value);
+    actualizarR(this.value);
 }
-function actualizar(nivelT) {
-    const nombre = document.getElementById('v1_jugador').innerText;
-    const nivelJ = document.getElementById('v1_nivel').innerText;
-    const tiempo = document.getElementById('v1_tiempo').innerText;
+function actualizarR(nivelT) {
+
+    l_ranking = [...l_rank];
+    //añado usuario actual
+    añadirUsuario();
+
+    //guardando lista segun el nivel
+
+    let l_nivel = [];
+    l_ranking.forEach(element => {
+        console.log(element.usuario, '--', element.tiempo);
+        if (element.nivel == nivelT) { l_nivel.push(element); }
+    });
+    //ordeno la lista por el tiemnpo
+    l_nivel.sort(((a, b) => a.tiempo - b.tiempo));
+
+    //actualizando datos tabla ranking
     const body = document.getElementsByTagName('tbody').item(0).children;
-    if (nivelJ == nivelT) {
-        body.item(0).children.item(2).innerHTML = nombre;
-        body.item(0).children.item(3).innerHTML = tiempo;
-    }
-    else {
-        body.item(0).children.item(2).innerHTML = 'Anonimo';
-        body.item(0).children.item(3).innerHTML = '59:59';
+    let filas = [].slice.call(body);
+    // console.log(filas);
+    for (let i = 0; i < filas.length; i++) {
+        filas[i].children.item(2).innerHTML = l_nivel[i].usuario;
+        filas[i].children.item(3).innerHTML = convertirTiempoMinutos(l_nivel[i].tiempo);
+
+
     }
 
+
+}
+function añadirUsuario() {
+
+    const usuario = document.getElementById('v1_jugador').innerText;
+    const nivel = document.getElementById('v1_nivel').innerText;
+    const tiempo = convertirTiempoSegundos(document.getElementById('v1_tiempo').innerText);
+    l_ranking.push({ usuario, nivel, tiempo });
+
+
+}
+function convertirTiempoMinutos(tiempo) {
+    if (tiempo == 0) { return '00:00' }
+    let minutos = Math.floor(parseInt(tiempo) / 60);
+    let segundos = (tiempo - minutos * 60);
+    if (minutos < 10) { minutos = '0' + minutos }
+    if (segundos < 10) { segundos = '0' + segundos }
+    return minutos + ':' + segundos;
+
+}
+function convertirTiempoSegundos(tiempo) {
+    let tiempoarray = tiempo.split(':');
+    let segundos = (parseInt(tiempoarray[0]) * 60) + (parseInt(tiempoarray[1]))
+    return segundos;
 
 }
 export default iniciarVista4
