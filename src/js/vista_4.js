c7907c1cbb2b8415f564a4890b1b614daa9e4ab2
sarariveros <sara.riverosd@gmail.com>
2022-11-06 20:17:45 -0500

    Actualizacion Ranking

--- src/js/vista_4.js
+++ src/js/vista_4.js
@@ -4,76 +4,91 @@ const l_rank = [
     {
         usuario: 'sara',
         nivel: '1',
-        tiempo: '320'
+        tiempo: '320',
+        fallos: '3'
     },
     {
         usuario: 'jeremy',
         nivel: '1',
-        tiempo: '195'
+        tiempo: '195',
+        fallos: '3'
     },
     {
         usuario: 'javier',
         nivel: '1',
-        tiempo: '490'
+        tiempo: '490',
+        fallos: '2'
     },
     {
         usuario: 'pablo',
         nivel: '1',
-        tiempo: '372'
+        tiempo: '372',
+        fallos: '3'
     },
     {
         usuario: 'gonzalo',
         nivel: '1',
-        tiempo: '250'
+        tiempo: '250',
+        fallos: '3'
     }, {
         usuario: 'ana',
         nivel: '2',
-        tiempo: '420'
+        tiempo: '420',
+        fallos: '2'
     },
     {
         usuario: 'jeremyz',
         nivel: '2',
-        tiempo: '560'
+        tiempo: '560',
+        fallos: '3'
     },
     {
         usuario: 'javiercito',
         nivel: '2',
-        tiempo: '685'
+        tiempo: '685',
+        fallos: '2'
     },
     {
         usuario: 'pablote',
         nivel: '2',
-        tiempo: '750'
+        tiempo: '750',
+        fallos: '3'
     },
     {
         usuario: 'gonza',
         nivel: '2',
-        tiempo: '890'
+        tiempo: '890',
+        fallos: '2'
     },
     {
         usuario: 'usu1',
         nivel: '3',
-        tiempo: '950'
+        tiempo: '950',
+        fallos: '3'
     },
     {
         usuario: 'usu2',
         nivel: '3',
-        tiempo: '1500'
+        tiempo: '1500',
+        fallos: '2'
     },
     {
         usuario: 'usu3',
         nivel: '3',
-        tiempo: '1260'
+        tiempo: '1260',
+        fallos: '2'
     },
     {
         usuario: 'usu4',
         nivel: '3',
-        tiempo: '1560'
+        tiempo: '1560',
+        fallos: '2'
     },
     {
         usuario: 'usu5',
         nivel: '3',
-        tiempo: '1350'
+        tiempo: '1350',
+        fallos: '2'
     },
 ]
 //cargando BD
@@ -127,6 +142,7 @@ function actualizarR(nivelT) {
     for (let i = 0; i < filas.length; i++) {
         filas[i].children.item(2).innerHTML = l_nivel[i].usuario;
         filas[i].children.item(3).innerHTML = convertirTiempoMinutos(l_nivel[i].tiempo);
+        filas[i].children.item(4).innerHTML = l_nivel[i].fallos;
 
 
     }
@@ -140,9 +156,10 @@ function añadirUsuario() {
 
     const usuario = ranking.nombre ?? '';
     const nivel = ranking.nivel ?? '';
+    const fallos = ranking.fallos ?? '';
     const tiempo = convertirTiempoSegundos(ranking.tiempo) + '' ?? '00:00';
 
-    l_ranking.push({ usuario, nivel, tiempo });
+    l_ranking.push({ usuario, nivel, tiempo, fallos });
 
 
 
@@ -157,9 +174,13 @@ function convertirTiempoMinutos(tiempo) {
 
 }
 function convertirTiempoSegundos(tiempo) {
-    let tiempoarray = tiempo.split(':');
-    let segundos = (parseInt(tiempoarray[0]) * 60) + (parseInt(tiempoarray[1]))
-    return segundos;
+    if(tiempo){
+        let tiempoarray = tiempo.split(':');
+        let segundos = (parseInt(tiempoarray[0]) * 60) + (parseInt(tiempoarray[1]))
+        return segundos;
+    }
+
+
 
 }
 export default iniciarVista4
